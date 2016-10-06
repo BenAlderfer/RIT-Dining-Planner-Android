6010cc6f5bc5afac70108cbbef4e60a7f29a14ff
Ben Alderfer <benalderfer974@gmail.com>
2016-10-06 00:11:02 -0400

    fixed current balance not correctly being restored

---
+++ app/src/test/java/com/alderferstudios/ritdebitsplitter/MainActivityUnitTest.java
@@ -0,0 +1,17 @@
+//package com.alderferstudios.ritdebitsplitter;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * To work on unit tests, switch the Test Artifact in the Build Variants view.
+// */
+//public class MainActivityUnitTest {
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        MainActivity act = new MainActivity();
+//
+//        assert(act.currentBalanceIsEntered());
+//    }
+//}
